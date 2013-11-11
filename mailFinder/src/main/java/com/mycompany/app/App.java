3fed71ad5c850edcdadfbe0b9664f1bc24dbd2d2
jsaguiar <jsaguiar@me.com>
2013-11-11 03:14:10 +0000

    lucene with mails properties and save to disk with search organized

--- mailFinder/src/main/java/com/mycompany/app/App.java
+++ mailFinder/src/main/java/com/mycompany/app/App.java
@@ -1,5 +1,7 @@
 package com.mycompany.app;
 
+
+import org.apache.lucene.analysis.core.WhitespaceAnalyzer;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.Document;
 import org.apache.lucene.index.DirectoryReader;
@@ -13,141 +15,29 @@ import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 import org.apache.lucene.analysis.standard.UAX29URLEmailTokenizer;
 
-import javax.mail.*;
 import java.io.File;
-import java.io.IOException;
-import java.util.Calendar;
-import java.util.Properties;
 
+import java.io.IOException;
 
 public class App {
 
 
 
-    static boolean textIsHtml = false;
-
-    /**
-     //     * Return the primary text content of the message.
-     */
-    static String getText(Part p) throws
-            MessagingException, IOException {
-        if (p.isMimeType("text/*")) {
-            String s = (String)p.getContent();
-            textIsHtml = p.isMimeType("text/html");
-            return s;
-        }
-
-        else if (p.isMimeType("multipart/*")) {
-            Multipart mp = (Multipart)p.getContent();
-            for (int i = 0; i < mp.getCount(); i++) {
-                String s = getText(mp.getBodyPart(i));
-                if (s != null)
-                    return s;
-            }
-        }
-
-        return null;
-    }
-
-
-    static String getMail(String [] fields){
-
-        String mail;
-
-
-
-        for (int i = 0; i< fields.length; i++){
-            if(fields[i].contains("@")){
-                mail = fields[i];
-                mail = mail.replace("<","");
-                mail = mail.replace(">","");
-                mail = mail.replace("\"","");
-                return mail.replace(",","");
-            }
-        }
-
-        return "Not found";
-    }
-
-
-
     public static void main(String[] args) throws Exception {
 
+        //importar mails da caixa
+        Mail mail = new Mail();
+        mail.importMail("mstor:mbox");
 
 
-        Properties props = new Properties();
-        props.setProperty("mail.store.protocol", "mstor");
-        props.setProperty("mstor.mbox.metadataStrategy", "none");
-        props.setProperty("mstor.mbox.cacheBuffers", "disabled");
-        props.setProperty("mstor.cache.disabled", "true");
-        props.setProperty("mstor.mbox.bufferStrategy", "mapped");
-        props.setProperty("mstor.metadata", "disabled");
-
-        props.setProperty("mstor.mbox.metadataStrategy", "xml");
-        Session session1 = Session.getDefaultInstance(props);
-
-        Session session = Session.getDefaultInstance(new Properties());
-
-        Store store = session.getStore(new URLName("mstor:mbox"));
-        store.connect();
-
-        System.out.println(store.isConnected());
-
-        Folder inbox = store.getDefaultFolder();  // no subfolder here; even if there is an Inbox, I get the same thing...
-        inbox.open(Folder.READ_ONLY);
-
-        Message [] messages = inbox.getMessages();
-
-        String body;
-        String from;
-        String to;
-        String subject;
-        String date;
-
-        Calendar cal = Calendar.getInstance();
-
-        Indexer index = new Indexer();
-       /* for (Message m : messages) {
-
-            body = getText(m);
-            to = getMail(InternetAddress.toString(m.getAllRecipients()).split(" "));
-            from = getMail(InternetAddress.toString(m.getReplyTo()).split(" "));
-            subject = m.getSubject();
-
-            cal.setTime(m.getSentDate());
-
-            date = String.valueOf(cal.get(Calendar.DAY_OF_MONTH)) + "/"
-                    + String.valueOf(cal.get(Calendar.MONTH)+1) + "/"
-                    + String.valueOf(cal.get(Calendar.YEAR)) + " "
-                    + String.valueOf(cal.get(Calendar.HOUR_OF_DAY)) + ":"
-                    + String.valueOf(cal.get(Calendar.MINUTE)) + ":"
-                    + String.valueOf(cal.get(Calendar.SECOND));
-
-            if(to.compareTo("Not found") == 0 || from.compareTo("Not found") == 0){
-                break;
-            }
-
-            index.addIndex(subject,body,date,from,to);
-            System.out.println("From: " + from);
-            System.out.println("To: " + to);
-            System.out.println("Subject: " + subject);
-            System.out.println("Body: " + body);
-            System.out.println("Date: " + date);
-
-        }     */
-
-        System.out.println("FINISH");
-        index.closeIndexer();
-
-
-        StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_45);
+        WhitespaceAnalyzer analyzer = new WhitespaceAnalyzer(Version.LUCENE_45);
 
         try{
             IndexReader reader = DirectoryReader.open(FSDirectory.open(new File("IndexerBD")));
             IndexSearcher searcher = new IndexSearcher(reader);
             TopScoreDocCollector collector = TopScoreDocCollector.create(5, true);
 
-            Query q = new QueryParser(Version.LUCENE_45, "data", analyzer).parse("30/09/2013");
+            Query q = new QueryParser(Version.LUCENE_45, "date", analyzer).parse("30 10 2013");
             System.out.println(q);
 
             searcher.search(q, collector);
---
+++ mailFinder/src/main/java/com/mycompany/app/Mail.java
@@ -0,0 +1,134 @@
+package com.mycompany.app;
+
+
+
+import javax.mail.*;
+import javax.mail.internet.InternetAddress;
+import java.io.IOException;
+import java.util.Calendar;
+import java.util.Properties;
+
+
+public class Mail {
+
+
+
+    static boolean textIsHtml = false;
+
+    /**
+     //     * Return the primary text content of the message.
+     */
+    static String getText(Part p) throws
+            MessagingException, IOException {
+        if (p.isMimeType("text/*")) {
+            String s = (String)p.getContent();
+            textIsHtml = p.isMimeType("text/html");
+            return s;
+        }
+
+        else if (p.isMimeType("multipart/*")) {
+            Multipart mp = (Multipart)p.getContent();
+            for (int i = 0; i < mp.getCount(); i++) {
+                String s = getText(mp.getBodyPart(i));
+                if (s != null)
+                    return s;
+            }
+        }
+
+        return null;
+    }
+
+
+    static String getMail(String [] fields){
+
+        String mail;
+
+
+
+        for (int i = 0; i< fields.length; i++){
+            if(fields[i].contains("@")){
+                mail = fields[i];
+                mail = mail.replace("<","");
+                mail = mail.replace(">","");
+                mail = mail.replace("\"","");
+                return mail.replace(",","");
+            }
+        }
+
+        return "Not found";
+    }
+
+
+
+    public void importMail (String mboxPath) throws Exception {
+
+
+        Properties props = new Properties();
+        props.setProperty("mail.store.protocol", "mstor");
+        props.setProperty("mstor.mbox.metadataStrategy", "none");
+        props.setProperty("mstor.mbox.cacheBuffers", "disabled");
+        props.setProperty("mstor.cache.disabled", "true");
+        props.setProperty("mstor.mbox.bufferStrategy", "mapped");
+        props.setProperty("mstor.metadata", "disabled");
+
+        props.setProperty("mstor.mbox.metadataStrategy", "xml");
+        Session session1 = Session.getDefaultInstance(props);
+
+        Session session = Session.getDefaultInstance(new Properties());
+
+
+        Store store = session.getStore(new URLName(mboxPath));
+        store.connect();
+
+        System.out.println(store.isConnected());
+
+        Folder inbox = store.getDefaultFolder();  // no subfolder here; even if there is an Inbox, I get the same thing...
+        inbox.open(Folder.READ_ONLY);
+
+        Message [] messages = inbox.getMessages();
+
+        String body;
+        String from;
+        String to;
+        String subject;
+        String date;
+
+        Calendar cal = Calendar.getInstance();
+
+        System.out.println("Start Importing....");
+        Indexer index = new Indexer();
+        for (Message m : messages) {
+
+            body = getText(m);
+            to = getMail(InternetAddress.toString(m.getAllRecipients()).split(" "));
+            from = getMail(InternetAddress.toString(m.getReplyTo()).split(" "));
+            subject = m.getSubject();
+
+            cal.setTime(m.getSentDate());
+
+            date = String.valueOf(cal.get(Calendar.DAY_OF_MONTH)) + "/"
+                    + String.valueOf(cal.get(Calendar.MONTH)+1) + "/"
+                    + String.valueOf(cal.get(Calendar.YEAR)) + " "
+                    + String.valueOf(cal.get(Calendar.HOUR_OF_DAY)) + ":"
+                    + String.valueOf(cal.get(Calendar.MINUTE)) + ":"
+                    + String.valueOf(cal.get(Calendar.SECOND));
+
+            if(to == null || from==null || to.compareTo("Not found") == 0 || from.compareTo("Not found") == 0 ){
+                break;
+            }
+
+            index.addIndex(subject,body,date,from,to);
+            /*System.out.println("From: " + from);
+            System.out.println("To: " + to);
+            System.out.println("Subject: " + subject);
+            System.out.println("Body: " + body);
+            System.out.println("Date: " + date);  */
+
+        }
+
+        index.closeIndexer();
+        System.out.println("Finished");
+
+    }
+
+}
